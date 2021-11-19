e0d418d7f8fc0d6e894ee6365bdc18aea56b6998
vaishnaviahaguru <vaishnavi.ahaguru@gmail.com>
2021-11-19 17:28:30 +0530

    Changes

---
+++ app/src/main/java/e/gcs/ahagurucbse/Test_Details_Activity.java
@@ -0,0 +1,14 @@
+package e.gcs.ahagurucbse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_Details_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test__details_);
+    }
+}
